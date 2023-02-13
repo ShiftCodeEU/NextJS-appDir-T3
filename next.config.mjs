f6158b403788c54e9cefc8ee0ab9641b6706872b
Jesse Koldewijn <jesse@iungo.nl>
2023-02-13 14:11:47 +0100

    fix: removal of old and broken code

--- next.config.mjs
+++ next.config.mjs
@@ -13,16 +13,5 @@ const config = {
   experimental: {
     appDir: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
 };
 export default config;

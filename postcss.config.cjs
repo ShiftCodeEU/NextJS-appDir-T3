25c519ff251f24ab748c5bc69f0730275a1ac43d
Jesse Koldewijn <jesse@iungo.nl>
2023-02-13 14:29:45 +0100

    update: config fix on postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 module.exports = {
   plugins: {
     tailwindcss: {},
